ae37942b033b50a15ef7ea0ca8ce1e9c3a216b51
Nguyen Manh Huy <huynm1103@gmail.com>
2020-04-13 11:26:53 +0900

    Array

--- practice.js
+++ practice.js
@@ -442,6 +442,28 @@ const pastaIndex = groceryList.indexOf('pasta');
 console.log(pastaIndex);
 
 //EX10 Arrays and Functions
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+
+console.log(concept);
+function removeElement(newArr){
+  newArr.pop();
+}
+
+removeElement(concept);
+console.log(concept);
+
+//EX11 Nested Arrays
+const numberClusters = [[1, 2], [3, 4], [5, 6]];
+
+const target = numberClusters[3][1];
+console.log(target);
+
 
 
 
